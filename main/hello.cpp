a9c2cd3aa7b58381f59b3ccbe86c9a997ac429b1
Mohammed Hassan <hassan020703@gmail.com>
2025-03-13 16:04:58 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Name: Mohammed Hassan \n SRN: PES2UG22CS317";
+  cout << "Change 1\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
